5c938639555947f6587b798819511b700226433a
Gordana Macura <gordanamacura@gmail.com>
2019-11-26 16:09:52 +0100

    committing current seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,15 @@ U1 = User.create(full_name: 'Charlotte Lin', address: 'Rheinsberger Str. 76, Ber
 
 U2 = User.create(full_name: 'Emily Johnson', address: 'Strelitzer Str. 51, Berlin', email: 'charlotte@example.com', password: '123456', password_confirmation: '123456')
 
-U3 = User.create(full_name: 'Frank McIntyre', address: 'Mauerpark, Berlin', email: 'frank@example.com', password: '123456', password_confirmation: '123456')
+U3 = User.create(full_name: 'Frank McIntyre', address: 'Elisabeth-Schwarzhaupt-Platz 1, Berlin', email: 'frank@example.com', password: '123456', password_confirmation: '123456')
 
 U4 = User.create(full_name: 'Ina Fellows', address: 'Wattstraße 6, Berlin', email: 'ina@example.com', password: '123456', password_confirmation: '123456')
 
 U5 = User.create(full_name: 'Linh Tran', address: 'Brunnenstraße 41, Berlin', email: 'linh@example.com', password: '123456', password_confirmation: '123456')
 
-U6 = User.create(full_name: 'Rahul Singh', address: 'Gustav-Meyer-Allee, Berlin', email: 'rahul@example.com', password: '123456', password_confirmation: '123456')
+U6 = User.create(full_name: 'Rahul Singh', address: 'Ackerstraße 40, Berlin', email: 'rahul@example.com', password: '123456', password_confirmation: '123456')
 
-U7 = User.create(full_name: 'Peter Thiem', address: 'Gleimviertel, Berlin', email: 'peter@example.com', password: '123456', password_confirmation: '123456')
+U7 = User.create(full_name: 'Peter Thiem', address: 'Ackerstraße 60, Berlin', email: 'peter@example.com', password: '123456', password_confirmation: '123456')
 
 U8 = User.create(full_name: 'Melanie Ciccioni', address: 'Oderberger Str. 33, Berlin', email: 'melanie@example.com', password: '123456', password_confirmation: '123456')
 
@@ -30,9 +30,26 @@ U10 = User.create(full_name: 'Giselle Hobosh', address: 'Bernauer Str. 89, Berli
 
 Dog 1
 url = "https://res.cloudinary.com/dlajprtba/image/upload/v1574434880/DoggieWalkie/Tinkerbell.jpg"
-d1 = Dog.create(name:'Tinkerbell’, user:User.last, breed:’Toy Poodle’, sex:’Female’, age:’Puppy’, hangouts:’,
-
+d1 = Dog.create(name:'Tinkerbell', user: U1, breed:'Toy Poodle', sex:'Female', age:'Puppy', hangouts:'Maurpark, Der Spree, Hundegaten Maurpark')
 
 Dog 2
-url = "https://res.cloudinary.com/dlajprtba/image/upload/v1574765745/DoggieWalkie/BeagleRyan.jpg”
-d2 = Dog.new(name:’Ryan’, user:User.last, breed:’Beagle’, sex:'Male’ age:’Puppy’, address: Strelitzer Str. 51, Berlin, b
+url = "https://res.cloudinary.com/dlajprtba/image/upload/v1574765745/DoggieWalkie/BeagleRyan.jpg"
+d2 = Dog.create(name:'Ryan', user: U2, breed:'Beagle', sex:'Male' age:'Puppy', hangouts:'Gorlitzer Park, Hundewiese Tempelhofer Feld')
+
+
+Activity1 = Activity.create(name:'Running')
+Activity2 = Activity.create(name:'Wrestling')
+Activity3 = Activity.create(name:'Playing Fetch')
+Activity4 = Activity.create(name:'Off-leash')
+Activity5 = Activity.create(name:'Casual Strolling')
+Activity6 = Activity.create(name:'Swimming')
+
+DogActivity.create(dog_id: 1, activity_id: Activity1)
+DogActivity.create(dog_id: 1, activity_id: Activity3)
+DogActivity.create(dog_id: 1, activity_id: Activity5)
+DogActivity.create(dog_id: 1, activity_id: Activity3)
+
+DogActivity.create(dog_id: 1, activity_id: Activity2)
+DogActivity.create(dog_id: 1, activity_id: Activity4)
+DogActivity.create(dog_id: 1, activity_id: Activity5)
+DogActivity.create(dog_id: 1, activity_id: Activity3)
