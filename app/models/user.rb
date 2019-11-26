dd5392020a0e4a30979375533677ec82997e495e
Charlotte Lin <charlotte.wanchen.lin@gmail.com>
2019-11-26 11:10:27 +0100

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :messages, dependent: :destroy
+  has_one :dog, :class_name => 'Dog', :foreign_key => 'user_id'
+
+  validates :full_name, presence: true
+  validates :address, presence: true
+  validates :lat, presence: true
+  validates :lng, presence: true
+  validates :photo, presence: true
 end
