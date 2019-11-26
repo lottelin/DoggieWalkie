8970a03d568f79c7f1d1c48e676e3677c0959e30
Petr Zaionchkovskiy <zaionchkovskiy@gmail.com>
2019-11-26 14:32:06 +0100

    created activities and join table

---
+++ app/models/dog_activity.rb
@@ -0,0 +1,4 @@
+class DogActivity < ApplicationRecord
+  belongs_to :activity
+  belongs_to :dog
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_110420) do
+ActiveRecord::Schema.define(version: 2019_11_26_132746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dog_activities", force: :cascade do |t|
+    t.bigint "activity_id"
+    t.bigint "dog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_dog_activities_on_activity_id"
+    t.index ["dog_id"], name: "index_dog_activities_on_dog_id"
+  end
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -46,5 +61,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_110420) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dog_activities", "activities"
+  add_foreign_key "dog_activities", "dogs"
   add_foreign_key "dogs", "users"
 end
