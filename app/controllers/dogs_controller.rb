1712158482894b3c01ed76aa1781a9ac54b99eeb
Petr Zaionchkovskiy <zaionchkovskiy@gmail.com>
2019-11-26 16:42:14 +0100

    started to work on dogs controller actions

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,5 +2,21 @@ class DogsController < ApplicationController
 
   def show
     @dog = Dog.find(params[:id])
+    # binding.pry
+    # @dog.dog_activities
   end
+
+  def edit
+    # @dog = Dog.find(params[:id])
+  end
+
+  def update
+    # @dog = Dog.find(params[:id])
+
+  end
+
+  def destroy
+
+  end
+
 end
